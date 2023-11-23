8ea1d39670413b6df5edd63914068e864e6a7789
Krishna Lokhande <133361024+Krisssss987@users.noreply.github.com>
2023-11-23 15:52:13 +0530

    forgot

--- src/app/login/forgot/forgot.component.ts
+++ src/app/login/forgot/forgot.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot.component.css']
 })
 export class ForgotComponent {
+  constructor(
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private router: Router,
+  ) {}
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  errorMessage = '';
+  loading:boolean = false;
+  loadingMessage: string  = "Submit";
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Email is required';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 
 }
--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, HostListener } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +10,5 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  
+
 }
