0a4cb346667e459b99cd15af23c462aa6dbc6948
Krishna Lokhande <133361024+Krisssss987@users.noreply.github.com>
2023-11-23 15:14:13 +0530

    login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  errorMessage = '';
+  loading: boolean = false;
+  loadingMessage: string = "Sign In";
 
+  constructor(
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Email is required';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'Password is required';
+    }
+    return this.password.hasError('minlength')
+      ? 'Password should be at least 8 characters long'
+      : '';
+  }
+
+
+  redirectUser(userType: string) {
+    if (userType === 'Standard' || userType === 'Admin') {
+      this.router.navigate(['/dash/temp']);
+    } else if (userType === 'Super Admin') {
+      this.router.navigate(['/sa/users']);
+    }
+  }
 }
