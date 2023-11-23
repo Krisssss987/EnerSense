5ead0f7860ba788c57b54544a311d82894422be9
snehal <kediasnehal73@gmail.com>
2023-11-23 17:48:51 +0530

    register

--- src/app/login/auth/auth.service.ts
+++ src/app/login/auth/auth.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  register(registerData: { companyName: string | null; companyEmail: string | null; contact: string | null; location: string | null; firstName: string | null; lastName: string | null; personalEmail: string | null; designation: string | null; password: string | null; }) {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
