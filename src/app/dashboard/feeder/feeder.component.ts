8990cfab1ff866b05dbab3eebb7f1ad700191acf
YatharthDeshpande <yatharthdeshpande724@gmail.com>
2023-12-04 17:33:35 +0530

    feeder meters

--- src/app/dashboard/feeder/feeder.component.ts
+++ src/app/dashboard/feeder/feeder.component.ts
@@ -12,7 +12,11 @@ export class FeederComponent {
   ngAfterViewInit() {
     Highcharts.chart('chartContainer', this.chartOptions);
     Highcharts.chart('pieContainer', this.pieChart);
-    Highcharts.chart('lineContainer', this.lineChart);
+    
+    Highcharts.chart('KVAguage', this.KVAguage);
+    Highcharts.chart('KWguage', this.KWguage);
+    Highcharts.chart('KVRguage', this.KVRguage);
+    Highcharts.chart('PFguage', this.PFguage);
   }
 
   constructor(
@@ -77,54 +81,291 @@ export class FeederComponent {
       pointPlacement: 0
     }],
   };
-  lineChart: Highcharts.Options = {
+ 
+  KVAguage: Highcharts.Options = {
     chart: {
-      type: 'line'
+      type: 'gauge',
+      plotBorderWidth: 0,
+      plotShadow: false,
+      height: '155vh',
+      margin: [0, 20, 20, 20],
+      backgroundColor: 'white',
+      style: {
+        border: 'none' // Remove stroke
+      }
     },
     title: {
-      text: 'fgfgg'
+      text: ''
     },
     credits: {
       enabled: false
     },
-    xAxis: {
-      categories: [
-        'Main PCC',
-        'HT Meter (S/Sn-4)',
-        'LT Meter (COB)',
-        'LT HILTOP INCOMER'
-      ]
+    pane: {
+      startAngle: -90,
+      endAngle: 89.9,
+      center: ['50%', '75%'],
+      size: '90%',
+      background: [{
+        backgroundColor: 'white'
+      }]
     },
-    yAxis: [{
+    yAxis: {
       min: 0,
-      title: {
-        text: ''
-      }
-    }, {
-      title: {
-        text: ''
+      tickPixelInterval: 72,
+      tickPosition: 'inside',
+      tickColor: 'white',
+      tickLength: 20,
+      tickWidth: 2,
+      minorTickInterval: null,
+      labels: {
+        distance: 20,
+        style: {
+          fontSize: '14px'
+        }
       },
-      opposite: true
-    }],
-    legend: {
-      shadow: false
-    },
-    tooltip: {
-      shared: true
+      lineWidth: 0,
+      plotBands: [{
+        from: 0,
+        to: 120,
+        color: '#55BF3B', // green
+        thickness: 20
+      }, {
+        from: 120,
+        to: 160,
+        color: '#DDDF0D', // yellow
+        thickness: 20
+      }, {
+        from: 160,
+        to: 50000000000000,
+        color: '#DF5353', // red
+        thickness: 20
+      }]
     },
     series: [{
-      name: 'Max Demand KVA',
-      color: 'rgba(165,170,217,1)',
-      data: [140, 90, 90, 90],
-      type: 'line'  // Specify the type of series as 'line'
-    }, {
-      name: 'Live Demand KVA',
-      color: 'rgba(126,86,134,.9)',
-      data: [150, 73, 20, 54],
-      type: 'line'  // Specify the type of series as 'line'
+      type: 'gauge',
+      name: '',
+      data: [240],
+      tooltip: {
+        valueSuffix: 'm'
+      },
+      dataLabels: {
+        format: '{y}  ',
+        borderWidth: 0,
+        color: (
+          Highcharts.defaultOptions.title &&
+          Highcharts.defaultOptions.title.style &&
+          Highcharts.defaultOptions.title.style.color
+        ) || '#333333',
+        style: {
+          fontSize: '16px'
+        }
+      },
+      dial: {
+        radius: '80%',
+        backgroundColor: 'gray',
+        baseWidth: 12,
+        baseLength: '0%',
+        rearLength: '0%'
+      },
+      pivot: {
+        backgroundColor: 'gray',
+        radius: 6
+      }
     }],
+  exporting: {
+    enabled: false // Disable the options button
+  }
   };
-  
+
+  KWguage: Highcharts.Options = {
+    chart: {
+      type: 'gauge',
+      plotBorderWidth: 0,
+      plotShadow: false,
+      height: '155vh',
+      margin: [0, 20, 20, 20],
+      backgroundColor: 'white',
+      style: {
+        border: 'none' // Remove stroke
+      }
+    },
+    title: {
+      text: ''
+    },
+    credits: {
+      enabled: false
+    },
+    pane: {
+      startAngle: -90,
+      endAngle: 89.9,
+      center: ['50%', '75%'],
+      size: '90%',
+      background: [{
+        backgroundColor: 'white'
+      }]
+    },
+    yAxis: {
+      min: 0,
+      tickPixelInterval: 72,
+      tickPosition: 'inside',
+      tickColor: 'white',
+      tickLength: 20,
+      tickWidth: 2,
+      minorTickInterval: null,
+      labels: {
+        distance: 20,
+        style: {
+          fontSize: '14px'
+        }
+      },
+      lineWidth: 0,
+      plotBands: [{
+        from: 0,
+        to: 120,
+        color: '#55BF3B', // green
+        thickness: 20
+      }, {
+        from: 120,
+        to: 160,
+        color: '#DDDF0D', // yellow
+        thickness: 20
+      }, {
+        from: 160,
+        to: 50000000000000,
+        color: '#DF5353', // red
+        thickness: 20
+      }]
+    },
+    series: [{
+      type: 'gauge',
+      name: '',
+      data: [240],
+      tooltip: {
+        valueSuffix: 'm'
+      },
+      dataLabels: {
+        format: '{y}  ',
+        borderWidth: 0,
+        color: (
+          Highcharts.defaultOptions.title &&
+          Highcharts.defaultOptions.title.style &&
+          Highcharts.defaultOptions.title.style.color
+        ) || '#333333',
+        style: {
+          fontSize: '16px'
+        }
+      },
+      dial: {
+        radius: '80%',
+        backgroundColor: 'gray',
+        baseWidth: 12,
+        baseLength: '0%',
+        rearLength: '0%'
+      },
+      pivot: {
+        backgroundColor: 'gray',
+        radius: 6
+      }
+    }],
+  exporting: {
+    enabled: false // Disable the options button
+  }
+  };
+
+  KVRguage: Highcharts.Options = {
+    chart: {
+      type: 'gauge',
+      plotBorderWidth: 0,
+      plotShadow: false,
+      height: '155vh',
+      margin: [0, 20, 20, 20],
+      backgroundColor: 'white',
+      style: {
+        border: 'none' // Remove stroke
+      }
+    },
+    title: {
+      text: ''
+    },
+    credits: {
+      enabled: false
+    },
+    pane: {
+      startAngle: -90,
+      endAngle: 89.9,
+      center: ['50%', '75%'],
+      size: '90%',
+      background: [{
+        backgroundColor: 'white'
+      }]
+    },
+    yAxis: {
+      min: 0,
+      tickPixelInterval: 72,
+      tickPosition: 'inside',
+      tickColor: 'white',
+      tickLength: 20,
+      tickWidth: 2,
+      minorTickInterval: null,
+      labels: {
+        distance: 20,
+        style: {
+          fontSize: '14px'
+        }
+      },
+      lineWidth: 0,
+      plotBands: [{
+        from: 0,
+        to: 120,
+        color: '#55BF3B', // green
+        thickness: 20
+      }, {
+        from: 120,
+        to: 160,
+        color: '#DDDF0D', // yellow
+        thickness: 20
+      }, {
+        from: 160,
+        to: 50000000000000,
+        color: '#DF5353', // red
+        thickness: 20
+      }]
+    },
+    series: [{
+      type: 'gauge',
+      name: '',
+      data: [240],
+      tooltip: {
+        valueSuffix: 'm'
+      },
+      dataLabels: {
+        format: '{y}  ',
+        borderWidth: 0,
+        color: (
+          Highcharts.defaultOptions.title &&
+          Highcharts.defaultOptions.title.style &&
+          Highcharts.defaultOptions.title.style.color
+        ) || '#333333',
+        style: {
+          fontSize: '16px'
+        }
+      },
+      dial: {
+        radius: '80%',
+        backgroundColor: 'gray',
+        baseWidth: 12,
+        baseLength: '0%',
+        rearLength: '0%'
+      },
+      pivot: {
+        backgroundColor: 'gray',
+        radius: 6
+      }
+    }],
+  exporting: {
+    enabled: false // Disable the options button
+  }
+  };
   pieChart:Highcharts.Options={
     chart: {
       type: 'pie'
@@ -179,6 +420,101 @@ export class FeederComponent {
       }]
     }]
   };
+  PFguage: Highcharts.Options = {
+    chart: {
+      type: 'gauge',
+      plotBorderWidth: 0,
+      plotShadow: false,
+      height: '155vh',
+      margin: [0, 20, 20, 20],
+      backgroundColor: 'white',
+      style: {
+        border: 'none' // Remove stroke
+      }
+    },
+    title: {
+      text: ''
+    },
+    credits: {
+      enabled: false
+    },
+    pane: {
+      startAngle: -90,
+      endAngle: 89.9,
+      center: ['50%', '75%'],
+      size: '90%',
+      background: [{
+        backgroundColor: 'white'
+      }]
+    },
+    yAxis: {
+      min: 0,
+      tickPixelInterval: 72,
+      tickPosition: 'inside',
+      tickColor: 'white',
+      tickLength: 20,
+      tickWidth: 2,
+      minorTickInterval: null,
+      labels: {
+        distance: 20,
+        style: {
+          fontSize: '14px'
+        }
+      },
+      lineWidth: 0,
+      plotBands: [{
+        from: 0,
+        to: 120,
+        color: '#55BF3B', // green
+        thickness: 20
+      }, {
+        from: 120,
+        to: 160,
+        color: '#DDDF0D', // yellow
+        thickness: 20
+      }, {
+        from: 160,
+        to: 50000000000000,
+        color: '#DF5353', // red
+        thickness: 20
+      }]
+    },
+    series: [{
+      type: 'gauge',
+      name: '',
+      data: [240],
+      tooltip: {
+        valueSuffix: 'm'
+      },
+      dataLabels: {
+        format: '{y}  ',
+        borderWidth: 0,
+        color: (
+          Highcharts.defaultOptions.title &&
+          Highcharts.defaultOptions.title.style &&
+          Highcharts.defaultOptions.title.style.color
+        ) || '#333333',
+        style: {
+          fontSize: '16px'
+        }
+      },
+      dial: {
+        radius: '80%',
+        backgroundColor: 'gray',
+        baseWidth: 12,
+        baseLength: '0%',
+        rearLength: '0%'
+      },
+      pivot: {
+        backgroundColor: 'gray',
+        radius: 6
+      }
+    }],
+  exporting: {
+    enabled: false // Disable the options button
+  }
+  };
+
   
 
   openSummary(): void {
