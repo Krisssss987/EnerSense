5dda71f2ad6bd3722d7710d690c6a1a01d274dc2
Krishna Lokhande <133361024+Krisssss987@users.noreply.github.com>
2024-03-18 09:31:18 +0530

    tod design

--- src/app/dashboard/tod-report/tod-report.component.ts
+++ src/app/dashboard/tod-report/tod-report.component.ts
@@ -18,31 +18,27 @@ import { Subscription } from 'rxjs';
   templateUrl: './tod-report.component.html',
   styleUrls: ['./tod-report.component.css']
 })
+
 export class TodReportComponent {
-CompanyId!:string|null;
+  CompanyId!:string|null;
   dataSource2: any;
-  displayedColumns: string[] = ['Device UID', 'Date Time'];
-  predefinedColumns: string[] = ['Device UID', 'Date Time'];
   dataSource = new MatTableDataSource<any>(ELEMENT_DATA);
-  panelOpenState = false;
   first_device!:string;
   data:any;
   currentDate: Date = new Date();
   startDate!: Date;
   endDate: Date = this.currentDate;
-  parameterOptions:any[] = [];
   errorMessage!: string;
   
   device_uid = new FormControl('', [Validators.required]);
-  parameters = new FormControl('', [Validators.required]);
   start_date = new FormControl('', [Validators.required]);
   end_date = new FormControl('', [Validators.required]);
   selectedDevice: any[] = [];
-  revertedArray: any[] = [];
   savedID: any;
   start!: string|null;
   end!: string|null;
   subscriptions: Subscription[] = [];
+  displayedColumns: string[] = ['date', 'ttl_kwh', 'ttl_kvah', 'a_kvah', 'a_kva', 'a_percent', 'b_kwh_6_to_9', 'b_kwh_12_to_18', 'b_kvah', 'b_kva', 'c_kvah', 'c_kva', 'd_kvah', 'd_kva', 'percent', 'solar_kwh', 'demand_charges', 'energy_charges', 'total_charges', 'unit_rate'];
 
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
@@ -55,7 +51,6 @@ CompanyId!:string|null;
 
   ngOnInit(){
     this.getUserDevices();
-    this.getParameters();
   }
 
   ngOnDestroy(){
@@ -75,110 +70,6 @@ CompanyId!:string|null;
       const subscription = this.DashDataService.deviceDetails(this.CompanyId).subscribe(
         (devices: any) => {
           this.dataSource2 = devices.getFeederData;
-          const savedParameters = sessionStorage.getItem('reportParameters');
-          const savedID = sessionStorage.getItem('reportDevice');
-          if (savedParameters === null || savedParameters === undefined || savedParameters === ''){
-            const reportParameters = ['KVA','KW','KVAR','PF','Current','Voltage N'];
-            this.selectedDevice = reportParameters;
-            const initialID = this.dataSource2[0].feederUid;    
-            sessionStorage.setItem('reportDevice', initialID);
-            this.savedID = sessionStorage.getItem('reportDevice');
-            const parametersArray: string[] = Array.isArray(reportParameters) ? reportParameters : [reportParameters];
-            this.displayedColumns = [...this.predefinedColumns, ...parametersArray];
-            this.displayedColumns = Array.from(new Set(this.displayedColumns));
-            this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-            const previousDate = new Date(this.currentDate);
-            previousDate.setDate(this.currentDate.getDate() - 1);
-            this.start = this.datePipe.transform(previousDate, 'yyyy-MM-dd')??'';
-            this.end = this.datePipe.transform(this.currentDate, 'yyyy-MM-dd')??'';  
-            sessionStorage.setItem('report_start_date',this.start)        
-            sessionStorage.setItem('report_end_date',this.end)   
-            this.startDate = new Date(this.start);   
-            
-            this.revertedArray = this.displayedColumns.map(str => {
-              return str.replace(/ /g, '_').toLowerCase();
-            });
-
-            const data = {
-              device_uid: initialID,
-              start_time: this.start,
-              end_time: this.end,
-              parameters: this.revertedArray,
-            }
-
-            const subscription = this.DashDataService.getreport(data).subscribe(
-              (data) => {
-                const actualData = data.data;
-                actualData.forEach((data: { date_time: string | number | Date | null; }) => {
-                  data.date_time = this.datePipe.transform(data.date_time, 'yyyy-MM-dd HH:mm:ss');
-                });
-                this.data=actualData;
-                
-                this.dataSource = new MatTableDataSource(actualData);
-                this.dataSource.paginator = this.paginator;
-              },
-              (error) => {
-                this.snackBar.open(
-                  error.error.message || 'Loading Data Failed.',
-                  'Dismiss',
-                  { duration: 2000 }
-                );
-                this.errorMessage = error.error.message || '';
-              }
-            );
-            this.subscriptions.push(subscription)
-          }
-          else {
-            const reportParameters = JSON.parse(savedParameters);
-            this.selectedDevice = reportParameters;
-            this.savedID = savedID;
-            const parametersArray: string[] = Array.isArray(reportParameters) ? reportParameters : [reportParameters];
-            this.displayedColumns = [...this.predefinedColumns, ...parametersArray];
-            this.displayedColumns = Array.from(new Set(this.displayedColumns));
-            this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-            const s = sessionStorage.getItem('report_start_date');
-            this.start = this.datePipe.transform(s, 'yyyy-MM-dd')??'';
-            const e = sessionStorage.getItem('report_end_date');
-            this.end = this.datePipe.transform(e, 'yyyy-MM-dd')??'';
-            if (s !== null) {
-              this.startDate = new Date(s);
-            } else {
-              this.startDate = new Date();
-            }
-
-            this.revertedArray = this.displayedColumns.map(str => {
-              return str.replace(/ /g, '_').toLowerCase();
-            });
-
-            const data = {
-              device_uid: savedID,
-              start_time: this.start,
-              end_time: this.end,
-              parameters: this.revertedArray,
-            }
-
-            const subscription = this.DashDataService.getreport(data).subscribe(
-              (data) => {
-                const actualData = data.data;
-                actualData.forEach((data: { date_time: string | number | Date | null; }) => {
-                  data.date_time = this.datePipe.transform(data.date_time, 'yyyy-MM-dd HH:mm:ss');
-                });
-                
-    this.dataSource = new MatTableDataSource(actualData);
-                this.data=actualData;
-                this.dataSource.paginator = this.paginator;
-              },
-              (error) => {
-                this.snackBar.open(
-                  error.error.message || 'Loading Data Failed.',
-                  'Dismiss',
-                  { duration: 2000 }
-                );
-                this.errorMessage = error.error.message || '';
-              }
-            );
-            this.subscriptions.push(subscription)
-          }
         },
         (error) => {
           this.snackBar.open('Error while fetching user devices!', 'Dismiss', {
@@ -188,33 +79,6 @@ CompanyId!:string|null;
       );
       this.subscriptions.push(subscription)
     }
-  }
-
-  getParameters() {
-    const subscription = this.DashDataService.getreportparameters().subscribe(
-      (parameters: string[]) => {
-        this.parameterOptions= parameters.map(str => {
-          let transformedStr = str.replace(/_/g, ' ');
-          transformedStr = transformedStr.replace(/\b\w/g, match => match.toUpperCase());
-          transformedStr = transformedStr.replace(/\b\w{1,4}\b/g, match => match.toUpperCase());
-          return transformedStr;
-        });
-      },
-      (error) => {
-        this.snackBar.open('Error while fetching Parameters!', 'Dismiss', {
-          duration: 2000
-        });
-        this.errorMessage = error.error.message || '';
-      }
-    );
-    this.subscriptions.push(subscription)
-  }
-
-  transformString(input: string): string {
-    let transformedStr = input.replace(/_/g, ' ');
-    transformedStr = transformedStr.replace(/\b\w/g, match => match.toUpperCase());
-    transformedStr = transformedStr.replace(/\b\w{1,4}\b/g, match => match.toUpperCase());
-    return transformedStr;
   }
 
   downloadCSV() {
@@ -278,51 +142,6 @@ CompanyId!:string|null;
   }
 
   applyFilter(){
-    if (this.device_uid.valid && this.start_date.valid && this.end_date.valid){
-      const selectedParameters: string | string[] = this.parameters.value!;
-      const serializedParameters = JSON.stringify(selectedParameters);
-      sessionStorage.setItem('reportParameters', serializedParameters);
-      sessionStorage.setItem('reportDevice', this.device_uid.value!);
-      const parametersArray: string[] = Array.isArray(selectedParameters) ? selectedParameters : [selectedParameters];
-      this.displayedColumns = [...this.predefinedColumns, ...parametersArray];
-      this.displayedColumns = Array.from(new Set(this.displayedColumns));
-      this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-      const formattedStartDate = this.datePipe.transform(this.start_date.value, 'yyyy-MM-dd')??'';
-      const formattedEndDate = this.datePipe.transform(this.end_date.value, 'yyyy-MM-dd')??'';
-      sessionStorage.setItem('report_start_date',formattedStartDate);
-      sessionStorage.setItem('report_end_date',formattedEndDate);
-
-      this.revertedArray = this.displayedColumns.map(str => {
-        return str.replace(/ /g, '_').toLowerCase();
-      });
-
-      const data = {
-        device_uid: this.device_uid.value,
-        start_time: formattedStartDate,
-        end_time: formattedEndDate,
-        parameters: this.revertedArray,
-      }
-
-      const subscription = this.DashDataService.getreport(data).subscribe(
-        (data) => {
-          const actualData = data.data;
-          actualData.forEach((data: { date_time: string | number | Date | null; }) => {
-            data.date_time = this.datePipe.transform(data.date_time, 'yyyy-MM-dd HH:mm:ss');
-          });
-          this.data=actualData;
-          this.dataSource = new MatTableDataSource(actualData);
-          this.dataSource.paginator = this.paginator;
-        },
-        (error) => {
-          this.snackBar.open(
-            error.error.message || 'Loading Data Failed.',
-            'Dismiss',
-            { duration: 2000 }
-          );
-        }
-      );
-      this.subscriptions.push(subscription)
-    }
   }
 }
 
